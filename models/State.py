e8e1ad1d42bd3f8c8b0f5619ea5a8e6ec6e1480a
solomon244 <slegesse2444@gmail.com>
2022-08-07 17:39:28 +0300

    update

--- models/State.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a State class """
-from models.base_model import BaseModel
-
-
-class State(BaseModel):
-    """ State class"""
-    pass
--- models/User.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a User class f"""
-from base_model import BaseModel
-
-
-class User(BaseModel):
-    """ User class"""
-    pass
