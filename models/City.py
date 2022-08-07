2cd0321d68a9f4fef535caa150635b8ecb1f1c7b
solomon244 <slegesse2444@gmail.com>
2022-08-07 17:33:29 +0300

    update

--- models/City.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a City class """
-from models.base_model import BaseModel
-
-
-class City(BaseModel):
-    """ City class"""
-    pass
--- models/Place.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""This module defines a Place class f"""
-from models.base_model import BaseModel
-
-
-class Place(BaseModel):
-    """ Place class"""
-    pass
